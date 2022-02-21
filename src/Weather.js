93ba568f386ae929aa14c462da964ff8920b783d
tarsilaschorr <89028181+tarsilaschorr@users.noreply.github.com>
2022-02-21 13:29:50 -0300

    started the forecast api call

--- src/Weather.js
+++ src/Weather.js
@@ -14,6 +14,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
@@ -57,7 +58,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
-  return (
-    <div className="row">
-      <div className="col">
-        <div className="Forecast-day">Tue</div>
-        <div className="Forecast-icon">
-          <WeatherIcon code="01d" />
-        </div>
-        <div className="Forecast-temp">
-          <span className="Forecast-temp-max">19º </span>
-          <span className="Forecast-temp-min">10º </span>
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="row">
+        <div className="col">
+          <div className="Forecast-day">Tue</div>
+          <div className="Forecast-icon">
+            <WeatherIcon code="01d" />
+          </div>
+          <div className="Forecast-temp">
+            <span className="Forecast-temp-max">19º </span>
+            <span className="Forecast-temp-min">10º </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "cd070e95ac9ddf93deb2685de9391443";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
